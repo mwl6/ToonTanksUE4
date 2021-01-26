d71e7b72089c32b34f7d8399f62f9253d5150d1a
Marc Lynch <marcwlynch@gmail.com>
2021-01-25 16:22:11 -0800

    Expanding tank functionality
    Topics covered:
    
    > Creating child classes in C++
    > Movement input

--- Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xBFBD345F,
-				0x1228C189,
+				0x6FE01076,
+				0xCDE2FDA6,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
