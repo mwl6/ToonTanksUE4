fd7234aa6edd3f2683e41a68e665f7e719d6d674
Marc Lynch <marcwlynch@gmail.com>
2021-01-31 23:24:26 -0800

    Adding projectiles
    Topics covered:
    
    > Creating and spawning actors
    > Working with projectiles

--- Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
+++ Intermediate/Build/Win64/UE4Editor/Inc/ToonTanks/ToonTanks.init.gen.cpp
@@ -21,8 +21,8 @@ void EmptyLinkFunctionForGeneratedCodeToonTanks_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0x7D6041BB,
-				0x138587E7,
+				0xD9A26CDA,
+				0x75FC1632,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UE4CodeGen_Private::ConstructUPackage(ReturnPackage, PackageParams);
